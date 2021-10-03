bb483327f090c35c6cd479efe2496be1e82b27e8
Lorran <lorranfoliveira@gmail.com>
2021-10-03 14:59:35 -0300

    Add deslocamentos nodais
    Cálculo dos deslocamentos nodais da referência batendo com o FTool

--- analise.jl
+++ analise.jl
@@ -1,8 +1,11 @@
 module Analise
 
 using LinearAlgebra
+using SparseArrays
 
-export No, Material, Elemento
+export No, Material, Elemento, Estrutura
+
+const num_gle = 6
 
 # =======================================================================
 """
@@ -13,7 +16,10 @@ x::Float64 -> Coordenada x.
 y::Float64 -> Coordenada y.
 fx::Float64 -> Força na direção x.
 fy::Float64 -> Força na direção y.
-mxy::Float64 -> Momento fletor.
+fxy::Float64 -> Momento fletor.
+rx::Bool -> Apoio na direção x. true para impedido e 'false' para livre.
+ry::Bool -> Apoio na direção y. true para impedido e 'false' para livre.
+rxy::Bool -> Apoio na direção xy (impede rotação). true para impedido e 'false' para livre.
 """
 struct No
     id::Int64
@@ -21,13 +27,16 @@ struct No
     y::Float64
     fx::Float64
     fy::Float64
-    mxy::Float64
+    fxy::Float64
+    rx::Bool
+    ry::Bool
+    rxy::Bool
 
-    function No(id, x, y, fx=0, fy=0, mxy=0)
+    function No(id, x, y, fx=0, fy=0, fxy=0, rx=false, ry=false, rxy=false)
         if id < 1
             throw(ArgumentError("A identificação do nó deve ser mais que 1!"))
         end
-        new(id, x, y, fx, fy, mxy)
+        new(id, x, y, fx, fy, fxy, rx, ry, rxy)
     end
 end
 
@@ -37,7 +46,7 @@ Calcula a distância entre dois nós.
 no₁::No -> Nó de referência 1.
 no₂::No -> Nó de referência 2.
 """
-function distancia(no₁::No, no₂::No)
+function distancia(no₁::No, no₂::No)::Float64
     return norm([(no₂.x - no₁.x), (no₂.y - no₁.y)])
 end
 
@@ -54,7 +63,14 @@ end
 Retorna o vetor de forças nodais.
 """
 function vetor_forcas(no::No)::Array{Float64}
-    return [no.fx, no.fy, no.mxy]    
+    return [no.fx, no.fy, no.fxy]    
+end
+
+"""
+Retorna o vetor de apoios nodais.
+"""
+function vetor_apoios(no::No)::Array{Bool}
+    return [no.rx, no.ry, no.rxy]
 end
 
 # =======================================================================
@@ -66,11 +82,11 @@ E: Módulo de Elasticidade.
 """
 struct Material
     id::Int64
-    v::Float64
     E::Float64
+    v::Float64
 
-    function Material(id, v, E)
-        if id < 1
+    function Material(id, E, v=0.2)
+            if id < 1
             throw(ArgumentError("O número de identificação do material não pode ser menor que 1!"))
         end
 
@@ -82,7 +98,7 @@ struct Material
             throw(ArgumentError("O Módulo de Elasticidade deve ser maior que 0!"))
         end
 
-        new(id, v, E)
+        new(id, E, v)
     end
 end
 
@@ -115,7 +131,7 @@ end
 """
 Comprimento do elemento.
 """
-function comprimento(elemento::Elemento)
+function comprimento(elemento::Elemento)::Float64
     return distancia(elemento.no₁, elemento.no₂)
 end
 
@@ -129,9 +145,16 @@ function gl_elem(elemento::Elemento)::Array{Int64}
 end
 
 """
+Retorna o vetor de forças atuantes nos graus de liberdade do elemento.
+"""
+function vetor_forcas(elemento::Elemento)::Array{Float64}
+    return vcat(vetor_forcas(elemento.no₁), vetor_forcas(elemento.no₂))
+end
+
+"""
 Ângulo de inclinação do elemento.
 """
-function angulo(elemento::Elemento)
+function angulo(elemento::Elemento)::Float64
     dx = elemento.no₂.x - elemento.no₁.x
     dy = elemento.no₂.y - elemento.no₁.y
 
@@ -141,31 +164,34 @@ end
 """
 Matriz de rotação do elemento.
 """
-function mat_rot(θ)::Matrix{Float64}
+function mat_rot(elemento::Elemento)::Array{Float64}
+    θ = angulo(elemento)
     s = sin(θ)
     c = cos(θ)
-    
-    return [[c, s, 0, 0]
-            [-s, c, 0, 0]
-            [0, 0, c, s]
-            [0, 0, -s, c]]
+
+    return [c s  0 0 0 0
+            -s c 0 0 0 0
+            0 0 1 0 0 0
+            0 0 0 c s 0
+            0 0 0 -s c 0
+            0 0 0 0 0 1]
 end
 
 """
 Matriz de rigidez local do elemento.
 """
-function ke(elemento::Elemento)
+function ke(elemento::Elemento)::Array{Float64}
     L = comprimento(elemento)
     E = elemento.material.E
     I = elemento.inercia
     A = elemento.area
 
-    k = [[E * A / L, 0, 0, -E * A / L, 0, 0]
-    [0, 12E * I / L^3, 6E * I / L^2, 0, -12E * I / L^3, 6E * I / L^2]
-    [0, 6E * I / L^2, 4E * I / L, 0, -6 * E * I / L^2, 2E * I / L]
-    [-E * A / L, 0, 0, E * A / L, 0, 0]
-    [0, -12E * I / L^3, -6E * I / L^2, 0, 12E * I / L^3, -6E * I / L^2]
-    [0, 6E * I / L^2, 2E * I / L, 0, -6 * I / L^2,4E * I / L]]
+    k = [E*A/L 0 0 -E*A/L 0 0
+         0 12E*I/L^3 6E*I/L^2 0 -12E*I/L^3 6E*I/L^2
+         0 6E*I/L^2 4E*I/L 0 -6*E*I/L^2 2E*I/L
+         -E*A/L 0 0 E*A/L 0 0
+         0 -12E*I/L^3 -6E*I/L^2 0 12E*I/L^3 -6E*I/L^2
+         0 6E*I/L^2 2E*I/L 0 -6*E*I/L^2 4E*I/L]
 
     return k
 end
@@ -173,14 +199,136 @@ end
 """
 Matriz de rigidez do elemento rotacionada para o sistema global.
 """
-function ke_rot(elemento::Elemento)
+function ke_rot(elemento::Elemento)::Array{Float64}
     k = ke(elemento)
-    θ = angulo(elemento)
-    r = mat_rot(θ)
+    r = mat_rot(elemento)
 
     return r' * k * r
 end
 
 # =======================================================================
+"""
+Implementa as propriedades de uma Estrutura.
+
+nos::Array{No} -> Nós que compõem a estrutura.
+elementos::Array{Elemento} -> Elementos que compõem a estrutura.
+forcas::Dict{Int64, Float64} -> Dicionário contendo os graus de liberdade (chaves) e as forças 
+                                neles atuantes (valores).
+apoios::Dict{Int64, Int8} -> Dicionário contendo os graus de liberdade (chaves) e o tipo de 
+                             vinculação
+"""
+struct Estrutura
+    nos::Array{No}
+    elementos::Array{Elemento}
+end
+
+"""
+Retorna o número de graus de liberdade da estrutura.
+"""
+function num_gls(estrutura::Estrutura)::Int64
+    return 3 * length(estrutura.nos)
+end
+
+"""
+Retorna os graus de liberdade livres.
+"""
+function gls_livres(estrutura::Estrutura)::Array{Int64}
+    return setdiff(1:num_gls(estrutura), vetor_apoios(estrutura))
+end
+
+"""
+Retorna o vetor de forças da estrutura.
+"""
+function vetor_forcas(estrutura::Estrutura, incluir_gls_impedidos::Bool=true)::Array{Float64}
+    forcas = zeros(num_gls(estrutura))
+    for no in estrutura.nos
+        gls = gl_no(no)
+        forcas[gls] = [no.fx, no.fy, no.fxy]
+    end
+
+    if incluir_gls_impedidos
+        return forcas
+    else
+        return forcas[gls_livres(estrutura)]
+    end
+end
+
+"""
+Retorna um vetor com os graus de liberdade impedidos.
+"""
+function vetor_apoios(estrutura::Estrutura)::Array{Int64}
+    apoios::Array{Int64} = []
+    for no in estrutura.nos
+        vaps = vetor_apoios(no)
+        gls = gl_no(no)
+
+        if any(vaps)
+            if vaps[1] == true
+                append!(apoios, gls[1]) 
+            end
+            if vaps[2] == true
+                append!(apoios, gls[2])
+            end
+            if vaps[3] == true
+                append!(apoios, gls[3])
+            end
+        end
+    end
+    return apoios
+end
+
+function ke_estrutura(estrutura::Estrutura)::SparseMatrixCSC
+    # Número de termos em cada matriz de rigidez de elementos.
+    global num_gle
+    apoios = vetor_apoios(estrutura)
+
+    num_it = length(estrutura.elementos) * num_gle^2
+
+    linhas = ones(Int64, num_it)
+    colunas = ones(Int64, num_it)
+    termos = zeros(num_it)
+
+    c = 1
+
+    for elem in estrutura.elementos
+        gle = gl_elem(elem)
+        ke = ke_rot(elem)
+
+        # Posições livres do elemento.
+        pos_livres = [i for (i, gl) in enumerate(gle) if gl ∉ apoios]
+
+        if length(pos_livres) > 0
+            for i in pos_livres
+                lin = gle[i] - count(x -> x < gle[i], apoios)
+                
+                for j in pos_livres
+                    linhas[c] = lin
+                    colunas[c] = gle[j] - count(x -> x < gle[j], apoios)
+                    termos[c] = ke[i, j]
+                    c += 1
+                end
+            end
+        end
+    end
+    k = sparse(linhas, colunas, termos)
+    dropzeros!(k)
+
+    return k
+end
+
+"""
+Retorna o vetor dos deslocamentos nodais.
+"""
+function deslocamentos(estrutura::Estrutura, incluir_gls_impedidos::Bool=true)::Array{Float64}
+    u = ke_estrutura(estrutura) \ vetor_forcas(estrutura, false)
+
+    if incluir_gls_impedidos
+        for i in vetor_apoios(estrutura)
+            insert!(u, i, 0)
+        end
+    end
+    return u
+end
+
 end
 
--- test.jl
+++ test.jl
@@ -12,12 +12,12 @@ using .Analise
     @test no1.y == 2
     @test no1.fx == 5
     @test no1.fy == 6
-    @test no1.mxy == 7
+    @test no1.fxy == 7
 
     no2 = No(1, 3, 2)
     @test no2.fx == 0
     @test no2.fy == 0
-    @test no2.mxy == 0
+    @test no2.fxy == 0
 
     ### Erros Fields ###
     @test_throws ArgumentError No(0, 0.2, 5e9)
@@ -34,26 +34,31 @@ end
 # =======================================================================
 @testset "Material" begin
     # Fields
+    m = Material(1, 5e9, 0.2)
+    #id
+    @test m.id == 1
+    @test m.v == 0.2
+    @test m.E == 5e9
 
     # id
-    @test_throws ArgumentError Material(0, 0.2, 1e9)
+    @test_throws ArgumentError Material(0, 1e9, 0.2)
     # E
-    @test_throws ArgumentError Material(1, 0.2, -1)
+    @test_throws ArgumentError Material(1, -1, 0.2)
     # v
-    @test_throws ArgumentError Material(1, -1, 1e9)
-    @test_throws ArgumentError Material(1, 1, 1e9)
+    @test_throws ArgumentError Material(1, 1e9, -1)
+    @test_throws ArgumentError Material(1, 1e9, 1)
 
     # Métodos
 end
 
 # =======================================================================
 @testset "Elemento" begin
-    no₁ = No(1, 3, 4)
-    no₂ = No(2, 5, 6)
-    mat = Material(1, 0.2, 5e9)
+    no₁ = No(1, 3, 4, 2, 3, 6)
+    no₂ = No(2, 5, 6, 5, 4, 7)
+    mat = Material(1, 5e9, 0.2)
 
     ### Fields ###
-    elem = Elemento(1, no₁, no₂, 10, 2, mat)   
+    elem = Elemento(1, no₁, no₂, 10, 2, mat)
 
     @test elem.id == 1
     @test elem.no₁ ≡ no₁
@@ -69,4 +74,58 @@ end
     @test_throws ArgumentError Elemento(1, no₁, no₂, 0, 2, Material)
     # inercia
     @test_throws ArgumentError Elemento(1, no₁, no₂, 10, 0, Material)
+
+    ### Métodos ###
+    @test Analise.comprimento(elem) ≈ 2.8284271247461903
+
+    @test Analise.gl_elem(elem) == [1, 2, 3, 4, 5, 6]
+
+    @test Analise.vetor_forcas(elem) == [2, 3, 6, 5, 4, 7]
+
+    @test Analise.angulo(elem) ≈ π / 4
+
+    # cos(π/4) == sin(π/4) == t
+    t = sin(π / 4)
+    kr = [t t 0 0 0 0
+          -t t 0 0 0 0
+          0 0 1 0 0 0
+          0 0 0 t t 0
+          0 0 0 -t t 0
+          0 0 0 0 0 1]
+    @test Analise.mat_rot(elem) ≈ kr
+
+
+end
+
+@testset "Estrutura" begin
+    # Unidades: kN, cm
+    no₁ = No(1, 0, 0, 0, 0, 0, true, true, true)
+    no₂ = No(2, 200, 200, 1, -1, 100)
+    no₃ = No(3, 400, 100, 0, 0, 0, false, true, false)
+    nos = [no₁, no₂, no₃]
+
+    m = Material(1, 2e5)
+
+    area = 13.796
+    inercia = 8.1052e2
+    elm₁ = Elemento(1, no₁, no₂, area, inercia, m)
+    elm₂ = Elemento(2, no₂, no₃, area, inercia, m)
+    elementos = [elm₁, elm₂]
+
+    estrutura = Estrutura(nos, elementos)
+
+    ### Fields ###
+    @test estrutura.nos ≡ nos
+    @test estrutura.elementos ≡ elementos
+
+    ### Métodos ###
+    @test Analise.num_gls(estrutura) == 9
+    
+    @test Analise.vetor_forcas(estrutura) == [0, 0, 0, 1, -1, 100, 0, 0, 0]
+    
+    @test Analise.vetor_apoios(estrutura) == [1, 2, 3, 8]
+
+    # t = Analise.ke_estrutura(estrutura)
+    t = Analise.deslocamentos(estrutura)
+
 end
